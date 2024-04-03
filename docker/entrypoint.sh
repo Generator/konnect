28eb3b88f332c6a1aaf817af2a053ec957929580
Generator <american.jesus.pt@gmail.com>
2024-04-03 12:55:18 +0000

    build from local path

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,10 +9,6 @@ addgroup -S -g $PGID $USERNAME
 adduser -S -D -H -h /app -u $PUID -G $USERNAME $USERNAME
 
 NAME=${NAME:-localhost}
-ADMIN_PORT=${ADMIN_PORT:-8080}
-DISCOVER_PORT=${DISCOVER_PORT:-1716}
-SERIVE_PORT=${SERVE_PORT:-1764}
-TRANSFER_PORT=${TRANSFER_PORT:-1763}
 MAX_TRANSFER_PORTS=${MAX_TRANSFER_PORTS:-3}
 CONFIG_DIR="/config"
 
@@ -21,11 +17,7 @@ chown -R "$PUID":"$PGID" "$CONFIG_DIR"
 
 echo "Starting with UID: $PUID, GID: $PGID"
 
-su-exec "$USERNAME" /usr/local/bin/konnectd --name "$NAME" \
-    --admin-port "$ADMIN_PORT" \
-    --discovery-port "$DISCOVER_PORT" \
-    --service-port "$SERIVE_PORT" \
-    --transfer-port "$TRANSFER_PORT" \
+su-exec "$USERNAME" /usr/local/bin/konnectd --name "${NAME:-konnect}" \
     --max-transfer-ports "$MAX_TRANSFER_PORTS" \
     --config-dir "$CONFIG_DIR" \
     "$@"
